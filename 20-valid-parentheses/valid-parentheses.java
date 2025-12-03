60b86030c1281fc76f3f876101f4cb323c28fc7d
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-03 19:37:36 +0530

    Time: 3 ms (73.15%) | Memory: 43.1 MB (35.90%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,37 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2!=0)return false;
-        Map<Character, Integer> val = new HashMap<>();
-
-        val.put('(', 1);
-        val.put('[', 2);
-        val.put('{', 3);
-        val.put(')', 4);
-        val.put(']', 5);
-        val.put('}', 6);
+        Stack<Character> stack = new Stack<>();
         
-        int[] arr = {0,4,5,6};
-
-        Stack<Integer> close = new Stack<>();
-        
-
-        for(char c: s.toCharArray()){
-            int curr= val.get(c);
-            
-            if(curr>3 ){
-                if (close.isEmpty())return false;
-                else{
-                    if(close.peek()!=curr)return false;
-                    else{
-                        close.pop();
-                    }
-                    
-                }
-            }else{
-                close.push(arr[curr]);
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else {
+                if (stack.isEmpty() || stack.pop() != c) return false;
             }
         }
-        if(!close.isEmpty())return false;
-        return true;
+        
+        return stack.isEmpty();
     }
 }
