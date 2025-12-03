62391159331f5191855c4f24d4d2a5dd96bc56b3
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-03 19:34:39 +0530

    Time: 4 ms (35.40%) | Memory: 44 MB (5.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0)return false;
+        Map<Character, Integer> val = new HashMap<>();
+
+        val.put('(', 1);
+        val.put('[', 2);
+        val.put('{', 3);
+        val.put(')', 4);
+        val.put(']', 5);
+        val.put('}', 6);
+        
+        int[] arr = {0,4,5,6};
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> close = new Stack<>();
+        
+
+        for(char c: s.toCharArray()){
+            int curr= val.get(c);
+            
+            if(curr>3 ){
+                if (close.isEmpty())return false;
+                else{
+                    if(close.peek()!=curr)return false;
+                    else{
+                        open.pop();
+                        close.pop();
+                    }
+                    
+                }
+            }else{
+                open.push(curr);
+                close.push(arr[curr]);
+            }
+        }
+        if(!open.isEmpty() || !close.isEmpty())return false;
+        return true;
+    }
+}
