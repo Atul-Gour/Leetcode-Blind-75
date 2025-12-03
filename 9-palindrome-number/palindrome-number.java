0829e2c6e56e9ad04d63b39e3de2280f01b00861
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-03 19:44:59 +0530

    Time: 8 ms (10.49%) | Memory: 46.2 MB (5.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        String s1 = String.valueOf(x);
+        String s2 = new StringBuilder(s1).reverse().toString();
+        if(s1.equals(s2))return true;
+        else return false;
+    }
+}
