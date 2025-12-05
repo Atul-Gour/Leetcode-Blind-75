cb9804498f6a2624141fe7e113672ef4c1ba2e54
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-05 17:51:10 +0530

    Time: 31 ms (4.96%) | Memory: 47.8 MB (8.90%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    TreeNode make(HashMap<Integer, Integer> map, int[] inorder, Stack<Integer> st, int left, int right){
+        
+        if(st.isEmpty() || left > right || right> inorder.length || left < 0)return null;
+        System.out.println(st.peek()+" left = "+left+" right = "+right);
+
+        int parentIndex =map.get(st.peek());
+        TreeNode curr = new TreeNode(st.pop());
+        if(st.isEmpty() || left==right)return curr;
+        System.out.println(" HII");
+        
+        int childIndex = map.get(st.peek());
+
+        if(childIndex>parentIndex && childIndex<=right)
+        curr.right= make(map, inorder, st, parentIndex+1 , right);
+
+        if(st.isEmpty() || left==right)return curr;
+        childIndex = map.get(st.peek());
+
+        if(childIndex<parentIndex && childIndex>=left){
+            curr.left= make(map, inorder, st, left , parentIndex-1);
+        }
+        return curr;
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int n=inorder.length;
+        Boolean [] visited = new Boolean[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st= new Stack<>();
+        for(int i=0;i<n; i++){
+            map.put(inorder[i],i);
+        }
+        for(int i: postorder){
+            st.push(i);
+        }
+        TreeNode root = make(map, inorder, st,0 ,n-1);
+        return root;
+
+    }
+}
