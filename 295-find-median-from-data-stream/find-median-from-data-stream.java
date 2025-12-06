ff8c0b3bb0bd8878c219343c0fd65d7273b96a39
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-12-06 21:35:51 +0530

    Time: 2331 ms (5.12%) | Memory: 112.2 MB (5.80%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,92 +1,68 @@
 class Node {
     int val;
-    Node next;
-    Node prev;
+    Node next, prev;
 
-    Node(int val) {
-        this.val = val;
-    }
+    Node(int v) { val = v; }
 }
+
 class MedianFinder {
-    Node head;
-    int n=0;
-    Node middle;
 
-    public MedianFinder() {
-        head= null;
-        middle = head;
-    }
+    Node head = null;
+    Node middle = null;
+    int n = 0;
 
-    void add(Node newNode){
-        Node temp = head;
-        Boolean isMiddle = false;
-        if(newNode.val >= middle.val){
-            temp=middle;
-            isMiddle = true;
-        }
+    public MedianFinder() {}
 
-        while(temp.next != null && newNode.val>temp.next.val){
-            temp=temp.next;
-        }
-        newNode.next= temp.next;
-        newNode.prev= temp;
-        temp.next = newNode;
-        if(newNode.next!=null)newNode.next.prev = newNode;
-        if(isMiddle){
-            if(n%2==0)middle= middle.next;
-        }else{
-            if(n%2==0)return;
-            middle= middle.prev;
-        }
-
-    }
-    
-    public void addNum(int num) {
+    // Insert into sorted DLL
+    private void insert(int num) {
         Node newNode = new Node(num);
 
         if (head == null) {
-            newNode.next = head;
-            head = newNode;
-            middle=head;
-            n++;
+            head = middle = newNode;
+            n = 1;
             return;
-        }else if(num< head.val){
+        }
+
+        // Insert at beginning
+        if (num < head.val) {
             newNode.next = head;
             head.prev = newNode;
-            head= newNode;
-            if(n%2!=0)middle = middle.prev;
+            head = newNode;
+
+            if (n % 2 == 1) middle = middle.prev;  // size increases from odd → even
             n++;
             return;
         }
-        else {
-            add(newNode);
-            n++;
+
+        Node temp = head;
+
+        // Move to correct spot
+        while (temp.next != null && temp.next.val <= num) {
+            temp = temp.next;
         }
-        // Node temp = head;
-        // while(temp!=null){
-        //     System.out.print(temp.val+" ");
-        //     temp=temp.next;
-        // }
-        // System.out.println(" end ");
+
+        newNode.next = temp.next;
+        newNode.prev = temp;
+        if (temp.next != null) temp.next.prev = newNode;
+        temp.next = newNode;
+
+        // Fix middle pointer after insertion
+        if (num < middle.val) {
+            if (n % 2 == 1) middle = middle.prev;
+        } else {
+            if (n % 2 == 0) middle = middle.next;
+        }
+
+        n++;
     }
-    
+
+    public void addNum(int num) {
+        insert(num);
+    }
+
     public double findMedian() {
-        double median =0;
-        if(n%2!=0){
-            median = middle.val;
-        }else{
-            
-            int a= middle.val;
-            int b= middle.next.val;
-            median = (b+a)/2.0;
-        }
-        return median;
+        if (n % 2 == 1) return middle.val;
+
+        return (middle.val + middle.next.val) / 2.0;
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
